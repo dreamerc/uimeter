052de60d16dfacbde53c0b910cb9f87bfedbca74
dreamerwolf.tw <dreamerwolf.tw@81da58b4-104c-11de-8784-2f9f11b02f98>
2009-10-18 11:01:32 +0000

    clean the script

--- trunk/trunk/src/stage2/pygame/run.py
+++ trunk/trunk/src/stage2/pygame/run.py
@@ -27,17 +27,12 @@ def main():
 
 # 圖形需存成 tga 加速影像轉換
     background = pygame.image.load("background.tga")
-    ball = pygame.image.load("test.tga")
-    ball2 = pygame.image.load("test2.tga")
-#fakewindow = pygame.image.load("fakewindow.tga")
     fakewindow = pygame.image.load("img/word.tga")
     minfakewindow = pygame.image.load("minfakewindow.tga")
 
 # 滑鼠位置
     pos = (0,0)
     lastpos = (0,0)
-
-#ballrect = ball.get_rect()
 
 # 載入外部視窗和初始化
     import Image
@@ -48,7 +43,7 @@ def main():
     hide = 1
 
 # Mouse 工作冷卻
-    cooldown = 0
+    mouse_timeout = 0
 
 # 顯示現在已經過秒數 (初始化)
     import time
@@ -57,7 +52,7 @@ def main():
     movie.play()
     pygame.time.wait(2500)
     
-    a = time.time()
+    begin_time = time.time()
 # 主程式正式開始, 若按 ESC 則跳出結束
     while not (pygame.key.get_pressed()[pygame.K_ESCAPE]):
         for event in pygame.event.get():
@@ -67,16 +62,8 @@ def main():
                 moves.append(event.rel)
         else: pass
 # 顯示現在狀態, 秒數和工作事件
-        print u'經過時間 : %f 秒, FPS : %f page/sec, 事件 : %s' % (time.time()-a, clock.get_fps(), event)
+        print u'經過時間 : %f 秒, FPS : %f page/sec, 事件 : %s' % (time.time()-begin_time, clock.get_fps(), event)
 
-## 乒乓球
-#    ballrect = ballrect.move(speed)
-#    if ballrect.left < 0 or ballrect.right > 640:
-#       speed[0] = -speed[0]
-#    if ballrect.top < 0 or ballrect.bottom > 480:
-#        speed[1] = -speed[1]
-
-#    screen.fill((0, 0, 0))
         screen.blit(background, (0, 0))
 #    screen.blit(ball,ballrect)
 
@@ -105,35 +92,19 @@ def main():
                             points[i] = (x, y)
 
             if max_x-100 <= mouse_pos[0] <= max_x and min_y <= mouse_pos[1] <= min_y+100:
-                if cooldown > 50:
+                if mouse_timeout > 50:
                     if hide == 1: hide = 0
                     elif hide == 0: hide = 1
                     else: pass
-                    cooldown = 0
+                    mouse_timeout = 0
                 else:
                     pass
             lastpos = pos
 
 ## 滑鼠冷卻
-        cooldown = cooldown + 1
-        if cooldown > 30000: cooldown = 0
+        mouse_timeout = mouse_timeout + 1
+        if mouse_timeout > 30000: mouse_timeout = 0
 
-
-## 旋轉球
-# 旋轉 30 度印出
-
-#   rotat = pygame.transform.rotozoom(ball2, angle,1)
-#   px = (rotat.get_width() - 50) / 2
-#   py = (rotat.get_height() - 50) / 2
-#   fix = 31
-#   screen.blit(rotat, (320-px-fix,200-py-fix))
-#   angle += 5
-#   angle %= 360
-
-
-## 旋轉滑鼠游標
-#   x, y = pos
-#   screen.blit(rotat, (x-px-fix, y-py-fix))
 
 # 將資料寫至螢幕
         pygame.display.flip()
@@ -143,7 +114,15 @@ def main():
 
 
 if __name__ == "__main__":
-#   try:
+
+    try:
+       import pygame
+    except:
+       print "pygame 尚未安裝"
+
+    try:
+       import Image
+    except:
+       print "Python Imaging Libary 尚未安裝"
+
     main()
-#   except:
-#       pass
