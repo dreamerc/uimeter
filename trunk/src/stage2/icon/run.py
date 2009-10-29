be17e8eefe2602bae877af7d1902bfe96822e548
dreamerwolf.tw <dreamerwolf.tw@81da58b4-104c-11de-8784-2f9f11b02f98>
2009-10-29 00:20:33 +0000

    增加 icon

--- trunk/src/stage2/icon/run.py
+++ trunk/src/stage2/icon/run.py
@@ -8,10 +8,6 @@ def main():
     import pygame,sys
     pygame.init()
 
-#   speed = [2,2]
-#   gray = (127,127,127)
-#   angle = 0.0
-
 # 標題
     pygame.display.set_caption('catch icon')
 
@@ -34,7 +30,8 @@ def main():
     print im.format, im.size, im.mode
     points = [(50, 50), (im.size[0]+50, 50), (im.size[0]+50, im.size[1]+50), (50, im.size[0]+50)]
     moves = []
-    hide = 1
+    hide = 0
+    fakeicon1 = pygame.image.load("img/program1.tga")
 
 # Mouse 工作冷卻
     cooldown = 0
@@ -74,32 +71,14 @@ def main():
 
         if pygame.mouse.get_pressed()[0]:
             if min_x <= mouse_pos[0] <= max_x and min_y <= mouse_pos[1] <= max_y:
+                screen.blit(fakeicon1, (points[0][0],points[0][1]))
                 print "mouse_on_0"
             if min_x+150 <= mouse_pos[0] <= max_x+150 and min_y <= mouse_pos[1] <= max_y:
+                screen.blit(fakeicon1, (points[0][0]+150,points[0][1]))
                 print "mouse_on_1"
             if min_x+300 <= mouse_pos[0] <= max_x+300 and min_y <= mouse_pos[1] <= max_y:
+                screen.blit(fakeicon1, (points[0][0]+300,points[0][1]))
                 print "mouse_on_2"
-            '''
-                if min_x < 0 : points = [(0, min_y), (im.size[0], min_y), (im.size[0], im.size[1]+ min_y), (0, im.size[0]+min_y)]
-                elif min_y < 0 : points = [(min_x, 0), (im.size[0]+min_x, 0), (im.size[0]+min_x, im.size[1]), (min_x, im.size[0]+min_y)]
-                elif max_x > framesize[0] : points = [(framesize[0]-im.size[0] , max_y-im.size[1]), (framesize[0] , max_y-im.size[1]), (framesize[0] , max_y), (framesize[0]-im.size[0] , max_y)]
-                elif max_y > framesize[1] : points = [(max_x-im.size[0] , framesize[1]-im.size[1]), (max_x , framesize[1]-im.size[1]), (max_x , framesize[1]), (max_x-im.size[0] , framesize[1])]
-                else:
-                    if lastpos != pos:
-                        for i in range(4):
-                            x = points[i][0] + moves[-1][0]
-                            y = points[i][1] + moves[-1][1]
-                            points[i] = (x, y)
-            if max_x-100 <= mouse_pos[0] <= max_x and min_y <= mouse_pos[1] <= min_y+100:
-                if cooldown > 50:
-                    if hide == 1: hide = 0
-                    elif hide == 0: hide = 1
-                    else: pass
-                    cooldown = 0
-                else:
-                    pass
-            lastpos = pos
-            '''
 ## 滑鼠冷卻
 
 
@@ -110,12 +89,16 @@ def main():
 # 將資料寫至螢幕
         pygame.display.flip()
         clock.tick(100)
-#       print clock.get_fps()
-#       pygame.display.set_caption("uimeter , fps: " + str(clock.get_fps()))
-
 
 if __name__ == "__main__":
-#   try:
+    try:
+       import pygame
+    except:
+       print "pygame 尚未安裝"
+
+    try:
+       import Image
+    except:
+       print "Python Imaging Libary 尚未安裝"
+
     main()
-#   except:
-#       pass
