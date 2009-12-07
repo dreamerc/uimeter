40decdfd0b965da12e5cb39b8056dfd0eda1c919
dreamerwolf.tw <dreamerwolf.tw@81da58b4-104c-11de-8784-2f9f11b02f98>
2009-12-07 08:14:12 +0000

    播放檔

---
+++ trunk/src/stage2/testway2/link.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#UIMeter
+#License: GNU General Public License v2
+#Author: Shan-Bin Chen <dreamerwolf.tw@gmail.com>
+
+print '按 ESC 可退出'
+import time
+time.sleep(5)
+
+import run1
+run1.main()
+
+time.sleep(5)
+
+import run2
+run2.main()
+
+time.sleep(5)
+
+import run3
+run3.main()
+
+time.sleep(20)
+
